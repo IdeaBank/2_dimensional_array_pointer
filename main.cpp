f86c241f1b0e6263d234a384fe7dcaea4f2be439
Woojin Lim <isp_lwj@naver.com>
2020-09-13 23:32:58 +0900

    main.cpp upgrade to mode 2

--- main.cpp
+++ main.cpp
@@ -3,16 +3,16 @@
 
 int main()
 {
-	int(*arr)[10] = (int(*)[10])malloc(sizeof(int) * 100);
+	int(*arr)[10] = (int(*)[10])malloc(sizeof(int) * 100), count = 0;
 
 	for (int(*i)[10] = arr; i != arr + 10; ++i)
 		for (int* j = *i; j != *i + 10; ++j)
-			*j = 0;
+			*j = ++count;
 
 	for (int(*i)[10] = arr; i != arr + 10; ++i)
 	{
 		for (int* j = *i; j != *i + 10; ++j)
-			printf("%d", *j);
+			printf("%4d", *j);
 		printf("\n");
 	}
 }
